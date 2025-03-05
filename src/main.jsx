7a2a438d375bce78d0d14f20702fd60362b5efd7
felipegrisales7 <84634893+felipegrisales7@users.noreply.github.com>
2025-03-05 17:52:45 -0500

    update test

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './my-app-element.js'
+import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    <App />
   </StrictMode>
 )
--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': '"production"',
+  },
   build: {
     lib: {
       entry: 'src/my-app-element.js',
       name: 'MyAppElement',
-      fileName: (format) => `my-app-element.${format}.js`,
+      formats: ['iife'], 
+      fileName: () => 'my-app-element.js',
     },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
+    rollupOptions: {},
   },
 })
